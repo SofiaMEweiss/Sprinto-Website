ae22f3916be14e8a136295dc4b62ff24e018deda
SofiaMEweiss <sofia.weiss@iths.se>
2021-09-20 14:59:18 +0200

    Cleaned up project files

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle `
+* {
+	box-sizing: border-box;
+	margin: 0px;
+	padding: 0px;
+	font-family: 'Encode Sans Expanded', sans-serif;
+	/* font-size: 12pt;
+	color: #000000; */
+}
+/* body, html {
+	min-height: 100vh;
+} */
+`
