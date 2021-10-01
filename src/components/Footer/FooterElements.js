ae589ff34ea82402af4a391376744f6774599876
SofiaMEweiss <sofia.weiss@iths.se>
2021-10-01 10:54:18 +0200

    Footer component finished

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,69 +1,49 @@
 import styled from "styled-components"
 import { Link } from "gatsby"
 
-export const FooterContainer = styled.footer`
+export const FooterWrapper = styled.footer`
+  padding: 48px 0 48px 0;
   background-color: #373e57;
   color: #fff;
-`
-
-export const FooterLinksContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* border: red solid 1px; */
-  padding: 48px;
 
   @media screen and (max-width: 820px) {
     padding-top: 32px;
   }
 `
-export const FooterLinksWrapper = styled.div`
+
+export const FooterContainer = styled.div`
   display: flex;
-  /* border: red solid 1px; */
   flex-direction: column;
+  align-items: center;
   padding: 0 0 1.5em 0;
-  align-items: center;
-`
-
-export const SocialMedia = styled.section`
-  max-width: 1000px;
-  width: 100%;
-  /* border: red solid 1px; */
-`
-
-export const SocialMediaWrap = styled.div`
-  display: flex;
-  align-items: center;
-  max-width: 1100px;
-  margin: 40px auto 0 auto;
-  flex-direction: column;
-  /* border: red solid 1px; */
-
-  @media screen and (max-width: 820px) {
-    flex-direction: column;
-  }
-`
-
-export const WebsiteRights = styled.small`
-  color: white;
-  /* border: red solid 1px; */
-  display: flex;
-  align-items: center;
-`
-
-export const SocialIconLink = styled.a`
-  color: #94e7d3;
-  font-size: 24px;
-  /* border: red solid 1px; */
 `
 
 export const FooterH3 = styled.h3`
+  padding: 0.2em;
   font-size: 1.5rem;
   font-weight: bold;
-  padding: 0.2em;
 `
 
 export const FooterP = styled.p`
-  line-height: 1.8rem;
-  font-size: 1.1rem;
   text-align: center;
+  font-size: 1.1rem;
+  line-height: 1.8rem;
+`
+
+export const SocialIconLink = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 0.2em;
+  color: #94e7d3;
+`
+
+export const WebsiteRights = styled.p`
+  display: flex;
+  justify-content: center;
+  padding: 32px 0 0 0;
+  color: #fff;
+
+  @media screen and (max-width: 820px) {
+    padding-top: 32px;
+  }
 `
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,53 +1,41 @@
 import React from "react"
 import { FaLinkedin } from "react-icons/fa"
-import { animateScroll as scroll } from "react-scroll"
 import {
+  FooterWrapper,
   FooterContainer,
-  FooterWrap,
-  FooterLinksContainer,
-  FooterLinksWrapper,
-  SocialMediaWrap,
-  WebsiteRights,
-  SocialIcons,
-  SocialIconLink,
   FooterH3,
   FooterP,
+  SocialIconLink,
+  WebsiteRights,
 } from "./FooterElements"
 
 const Footer = () => {
-  const toggleHome = () => {
-    scroll.scrollToTop()
-  }
   return (
-    <FooterContainer>
-      <FooterLinksContainer>
-        <FooterLinksWrapper>
-          <FooterH3>KOM FÖRBI</FooterH3>
-          <FooterP>
-            Magnusladulåsgatan {<br />}
-            1118 63 Stockholm
-          </FooterP>
-        </FooterLinksWrapper>
-        <FooterLinksWrapper>
-          <FooterH3>HÖR AV DIG</FooterH3>
-          <FooterP>Info@sprinto.se{<br />} 070-000 00 00</FooterP>
-        </FooterLinksWrapper>
-        <FooterLinksWrapper>
-          <SocialIconLink
-            href="//www.linkedin.com/company/sprinto/about/"
-            target="_blank"
-            aria-label="Linkedin"
-          >
-            <FaLinkedin size={35} />
-          </SocialIconLink>
-        </FooterLinksWrapper>
-        <SocialMediaWrap>
-          <WebsiteRights>
-            Sprinto © {new Date().getFullYear()}All right reserved.
-          </WebsiteRights>
-        </SocialMediaWrap>
-      </FooterLinksContainer>
-    </FooterContainer>
+    <FooterWrapper>
+      <FooterContainer>
+        <FooterH3>KOM FÖRBI</FooterH3>
+        <FooterP>
+          Magnus Ladulåsgatan 1{<br />}
+          118 65 Stockholm
+        </FooterP>
+      </FooterContainer>
+      <FooterContainer>
+        <FooterH3>HÖR AV DIG</FooterH3>
+        <FooterP>Info@sprinto.se{<br />} 070-000 00 00</FooterP>
+      </FooterContainer>
+      <FooterContainer>
+        <SocialIconLink
+          href="//www.linkedin.com/company/sprinto/about/"
+          target="_blank"
+          aria-label="Linkedin"
+        >
+          <FaLinkedin size={35} />
+        </SocialIconLink>
+      </FooterContainer>
+      <WebsiteRights>
+        Sprinto © {new Date().getFullYear()}All right reserved.
+      </WebsiteRights>
+    </FooterWrapper>
   )
 }
 
